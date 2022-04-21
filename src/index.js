455dbd02403762dd9acd9db28daeaba62549fff7
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-04-20 20:00:13 -0400

    b

--- src/index.js
+++ src/index.js
@@ -50,8 +50,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <>
     <Provider store={store}>
-      {/* <App /> */}
-      <div>hi</div>
+      <App />
     </Provider>
   </>,
   document.getElementById("root")
