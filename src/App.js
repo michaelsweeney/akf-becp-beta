9f38dc381b9c02f18e4b34f885f3b86d5f4a9ea3
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-04-21 09:14:04 -0400

    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { conn } from "./store/connect";
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import { conn } from "./store/connect";
 
-import { makeStyles } from "@material-ui/styles";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { makeStyles } from "@material-ui/styles";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import * as api from "./apicalls";
-import * as d3 from "d3";
-import PlotContainer from "./components/plots/plotcontainer";
-import CaseControls from "./components/casecontrols";
-import GlobalControls from "./components/globalcontrols";
+// import * as api from "./apicalls";
+// import * as d3 from "d3";
+// import PlotContainer from "./components/plots/plotcontainer";
+// import CaseControls from "./components/casecontrols";
+// import GlobalControls from "./components/globalcontrols";
 // import { LoadingSpinner } from "./components/loadingspinner";
 // import { LoadingScreenError } from "./components/loadingerrorscreen";
 // import { Header } from "./components/header";
