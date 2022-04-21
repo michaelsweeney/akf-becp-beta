f4eca8d34701348890be00e52e1490ba0f8d120f
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-04-20 20:22:57 -0400

    d

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 // import "./App.css";
 // import { conn } from "./store/connect";
 
-import { makeStyles } from "@material-ui/styles";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { makeStyles } from "@material-ui/styles";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // import * as api from "./apicalls";
-import * as d3 from "d3";
+// import * as d3 from "d3";
 import PlotContainer from "./components/plots/plotcontainer";
 // import PlotControls from "./components/plotcontrols";
 import CaseControls from "./components/casecontrols";
