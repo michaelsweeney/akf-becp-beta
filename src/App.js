19d444aaeaa17211973095ffe81cfd092e3f8f19
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2022-04-22 10:57:48 -0400

    heroku-debug

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import * as d3 from "d3"; // << NO ERROR
 // import PlotContainer from "./components/plots/plotcontainer"; //<< CAUSES ERROR
 // import CaseControls from "./components/casecontrols"; //<< CAUSES ERROR
 // import GlobalControls from "./components/globalcontrols"; // << CAUSES ERROR
-// import { LoadingSpinner } from "./components/loadingspinner"; // << CAUSES ERROR
+import { LoadingSpinner } from "./components/loadingspinner"; // << CAUSES ERROR
 import { LoadingScreenError } from "./components/loadingerrorscreen"; // << NO ERROR
 import { Header } from "./components/header"; // << NO ERROR
 // import ResultsTable from "./components/resultstable"; // << CAUSES ERROR
